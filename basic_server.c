5113ea9ce25b3205941f6b59f97b67dd830ef3ff
DevMMing <mming@stuy.edu>
2018-12-19 14:47:30 -0500

    added some stuff

--- basic_server.c
+++ basic_server.c
@@ -5,6 +5,19 @@ int main() {
 
   int to_client;
   int from_client;
-
+  char msg[BUFSIZ];
+  char str[BUFSIZ];
   from_client = server_handshake( &to_client );
+  mkfifo(from_client);
+  while(1){
+	printf("Input message to server: ");
+   	scanf("%s", str);
+	if(!strcmp(str,"exit")){
+	printf("closing server");
+	break;
+	}
+	read(to_client,msg,sizeof(msg));
+	
+	write();
+  }
 }
--- client.c
+++ client.c
@@ -1,10 +1,16 @@
 #include "pipe_networking.h"
 
-
+static void sighandler( int signum){
+  if(signum == SIGINT){
+    
+  }
+}
 int main() {
 
   int to_server;
   int from_server;
 
-  from_server = client_handshake( &to_server );
+  from_server = client_handshake( &to_server );	
+  while(1){
+  }
 }
