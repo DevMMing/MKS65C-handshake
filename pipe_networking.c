429464ba1a266ced44d1debbdbb308db9b76c51b
DevMMing <mming@stuy.edu>
2018-12-19 23:01:28 -0500

    update server and client

--- pipe_networking.c
+++ pipe_networking.c
@@ -4,92 +4,79 @@
 /*=========================
   server_handshake
   args: int * to_client
-
   Performs the client side pipe 3 way handshake.
   Sets *to_client to the file descriptor to the downstream pipe.
-
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  int client_to_server;
-   char *myfifo = "/tmp/client_to_server_fifo";
+	printf("[server]-creating well known pipe: well_known_pipe\n");
+	mkfifo("well_known_pipe", 0777);
+  	int wkp = open("well_known_pipe", O_RDONLY);
+	if(wkp == -1){
+	  printf("[server]-error opening Well Known Pipe: %s\n", strerror(errno));
+	  exit(0);
+	}
+	char * client = malloc(BUFFER_SIZE);
+  	read(wkp, client, BUFFER_SIZE);
+  	printf("[server]-received message from client: %s\n", client);
+	remove("well_known_pipe");
+	printf("[server]-removed well known pipe.\n");
 
-   int server_to_client;
-   char *myfifo2 = "/tmp/server_to_client_fifo";
+	printf("[server]-sending message [%s] to client: %s\n",ACK,client);
 
-   char buf[BUFSIZ];
+	*to_client = open(client, O_WRONLY);
+	if(*to_client == -1){
+	  printf("[server]-error opening %s: %s\n", client, strerror(errno));
+	  exit(0);
+	}
+	write(*to_client, ACK, BUFFER_SIZE);
 
-   /* create the FIFO (named pipe) */
-   mkfifo(myfifo, 0777);
-   mkfifo(myfifo2, 0777);
+	char * ack_confirm = malloc(BUFFER_SIZE);
+	read(wkp,ack_confirm,BUFFER_SIZE);
+	printf("[server]-recieved [%s] from client: %s.\n",ack_confirm,client);
 
-   /* open, read, and display the message from the FIFO */
-   client_to_server = open(myfifo, O_RDONLY);
-   server_to_client = open(myfifo2, O_WRONLY);
+	free(client);
+	free(ack_confirm);
 
-   printf("Server ON.\n");
-
-   while (1)
-   {
-      read(client_to_server, buf, BUFSIZ);
-
-      if (strcmp("exit",buf)==0)
-      {
-         printf("Server OFF.\n");
-         break;
-      }
-
-      else if (strcmp("",buf)!=0)
-      {
-         printf("Received: %s\n", buf);
-         printf("Sending back...\n");
-         write(server_to_client,buf,BUFSIZ);
-      }
-
-      /* clean buf from any data */
-      memset(buf, 0, sizeof(buf));
-   }
-
-   close(client_to_server);
-   close(server_to_client);
-
-   remove(myfifo);
-   remove(myfifo2);
-   *to_client=server_to_client;
-   return client_to_server;
+	return wkp;
 }
 
 
 /*=========================
   client_handshake
   args: int * to_server
-
   Performs the client side pipe 3 way handshake.
   Sets *to_server to the file descriptor for the upstream pipe.
-
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-	   int client_to_server;
-   char *myfifo = "/tmp/client_to_server_fifo";
-
-   int server_to_client;
-   char *myfifo2 = "/tmp/server_to_client_fifo";
-
-   char str[BUFSIZ];
-   printf("Input message to server: ");
-   scanf("%s", str);
-
-
-   /* write str to the FIFO */
-   client_to_server = open(myfifo, O_WRONLY);
-   server_to_client = open(myfifo2, O_RDONLY);
-   write(client_to_server, str, sizeof(str));
-   read(server_to_client,str,sizeof(str));
-   printf("...received from the server: %s\n",str);
-   close(client_to_server);
-   close(server_to_client);
-
-	*to_server=client_to_server;
-   return server_to_client;
+	printf("[client]-opening well_known_pipe\n");
+	*to_server = open("well_known_pipe", O_WRONLY);
+	if(*to_server == -1){
+	  printf("[client]-error opening well_known_pipe: %s\n", strerror(errno));
+	  exit(0);
 	}
+	printf("[client]-opening downstream pipe\n");
+
+	char * pipe_name = malloc(HANDSHAKE_BUFFER_SIZE);
+	sprintf(pipe_name,"%d",getpid());
+  	mkfifo(pipe_name, 0777);
+  	write(*to_server, pipe_name, BUFFER_SIZE);
+	printf("[client]-sending downstream pipe name [%s] through well_known_pipe\n",pipe_name);
+
+	int downstream = open(pipe_name, O_RDONLY);
+	if(downstream == -1) {
+	  printf("[client]-error opening downstream pipe: %s\n", strerror(errno));
+	  exit(0);
+	}
+
+	char * server_ack = malloc(BUFFER_SIZE);
+	read(downstream, server_ack, BUFFER_SIZE);
+	printf("[client]-received message from server: %s\n", server_ack);
+	free(server_ack);
+	remove(pipe_name);
+
+	printf("[client]-sending message to server: %s\n" ,ACK);
+	write(*to_server, ACK, BUFFER_SIZE);
+	return downstream;
+}
